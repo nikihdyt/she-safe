cbb475b315a0489b3e963f1c78fb0a43aa830d06
nikihdyt <nikihidayati20@gmail.com>
2023-04-12 15:28:24 +0700

    fix(sensor): apply light sensor on fragment

--- app/src/main/java/com/example/android/shesafe/HostFragmentActivity.java
+++ app/src/main/java/com/example/android/shesafe/HostFragmentActivity.java
@@ -2,12 +2,17 @@ package com.example.android.shesafe;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
@@ -16,14 +21,21 @@ import com.google.gson.Gson;
 
 import java.util.LinkedList;
 
-public class HostFragmentActivity extends AppCompatActivity {
+public class HostFragmentActivity extends AppCompatActivity implements SensorEventListener {
 
     private LinkedList<String[]> mContactList = new LinkedList<>();
+
+    private SensorManager mSensorManager;
+    private Sensor mSensorLight;
+    private CardView screen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_host_fragment);
+
+//        INITIALIZING UI ELEMENTS
+        screen = (CardView) findViewById(R.id.screen);
 
         // Enable the up button on the action bar
         ActionBar actionBar = getSupportActionBar();
@@ -47,18 +59,10 @@ public class HostFragmentActivity extends AppCompatActivity {
         transaction.replace(R.id.fragment_container, fragment);
         transaction.commit();
 
-        // TODO: retrieve the contact list from the fragment then store it to sharedPreference
-
-//        GET contact list from ContactsFragment
-//        ContactsFragment contactsFragment = (ContactsFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_container);
-//        LinkedList<String[]> contactList = contactsFragment.getContactList();
-        // store the contact list in the sharedPreference
-//        if (mContactList != null) {
-//            storeLinkedList(mContactList);
-//            Log.e("HostFragmentActivity", "contactList retrived from contactFragmnet: " + mContactList.toString());
-//        } else {
-//            Log.e("HostFragmentActivity", "contactList is null");
-//        }
+//        START HANDLE SENSOR
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mSensorLight = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+//        END HANDLE SENSOR
 
     }
 
@@ -74,13 +78,36 @@ public class HostFragmentActivity extends AppCompatActivity {
         }
     }
 
-//    private void storeLinkedList(LinkedList<String[]> linkedList) {
-//        SharedPreferences sharedPreferences = getSharedPreferences("mySharedPreferences", Context.MODE_PRIVATE);
-//        SharedPreferences.Editor editor = sharedPreferences.edit();
-//        Gson gson = new Gson();
-//        String linkedListJson = gson.toJson(linkedList);
-//        editor.putString("linkedList", linkedListJson);
-//        editor.apply();
-//    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (mSensorLight != null) {
+            mSensorManager.registerListener(this, mSensorLight,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_LIGHT) {
+            float lightLevel = event.values[0];
+            float alpha = 1 - (lightLevel / 50000f);
+            alpha = Math.max(alpha, 0f);
+            alpha = Math.min(alpha, 0.8f);
+            screen.setAlpha(alpha);
+
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
 
 }
